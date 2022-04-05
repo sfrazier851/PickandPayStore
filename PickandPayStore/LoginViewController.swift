2fdda1c7e922c35bca9d386c4735349c230f6a56
sfrazier851 <spencer851@revature.net>
2022-04-05 09:02:02 -0500

    add outlets for login screen

--- PickandPayStore/LoginViewController.swift
+++ PickandPayStore/LoginViewController.swift
@@ -11,6 +11,17 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var backToHomeButton: UIButton!
     
+    @IBOutlet weak var mobileNumberTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
