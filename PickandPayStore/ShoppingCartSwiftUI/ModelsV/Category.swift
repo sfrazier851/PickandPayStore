35c87c971bbe117f88c993c4919f8e464d257c55
Costin79 <costpopr@protonmail.com>
2022-04-05 17:00:47 -0500

    Added pick categories view

--- PickandPayStore/ShoppingCartSwiftUI/ModelsV/Category.swift
+++ PickandPayStore/ShoppingCartSwiftUI/ModelsV/Category.swift
@@ -17,7 +17,6 @@ struct Category: Identifiable{
 // Dummy data. Created so it can be used for display.
 // The Product objects are initialized without the id property cuz the id is already set up in the model.
 
-//var productList = [Category(name: "", image: "bear", price: 9.99),
-                   //Product(name: "Circles", image: "circles", price: 16.98),
-                   //Product(name: "Dino", image: "dino", price: 29.97),
-                   //Product(name: "Unicorn", image: "unicorn", price: 3.99),]
+/*var categoryList = [Category(name: "spaceships", image: "x-wing"),
+                    Category(name: "ground", image: "speeder" ),
+                    Category(name: "droids", image: "buddy" )]*/
---
+++ PickandPayStore/ShoppingCartSwiftUI/Views/PickCategoryView.swift
@@ -0,0 +1,48 @@
+//
+//  PickCategoryView.swift
+//  PickandPayStore
+//
+//  Created by costin popescu on 4/5/22.
+//
+
+import SwiftUI
+
+struct PickCategoryView: View {
+    
+    @StateObject var productsManager: ProductsManager = ProductsManager()
+    
+    var body: some View {
+        
+        NavigationView {
+            ScrollView{
+                    Text("Pick a Category").bold()
+                    VStack{
+                        //Text("Pick a Category")
+                        ForEach(productsManager.categories, id: \.id){ category in
+                            NavigationLink(destination: CategoryContentView(category: category, productsList: productsManager.getProductsOfCategory(categoryName: category.name))
+                                    .environmentObject(productsManager))
+                                {
+                                    CategoryCard(category: category)
+                                        .environmentObject(productsManager)
+                            }
+                    }
+                    .padding()
+                }
+                .toolbar(content:{
+                    ToolbarItem(placement: .principal, content: {
+                            Text("Millennium Technologies")
+                            .font(.title).fontWeight(.bold)
+                        })
+                })
+        }
+    }
+    
+}
+}
+
+struct PickCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickCategoryView()
+    }
+}
+
