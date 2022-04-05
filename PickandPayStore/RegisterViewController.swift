a3d8295e31c8edafb2ded00a088329f62f5d279a
sfrazier851 <spencer851@revature.net>
2022-04-05 09:00:25 -0500

    add outlets for register screen

--- PickandPayStore/RegisterViewController.swift
+++ PickandPayStore/RegisterViewController.swift
@@ -11,10 +11,23 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var backToHomeButton: UIButton!
     
+    @IBOutlet weak var mobileNumberTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
-    
     
     @IBAction func backToHomeButtonTapped(_ sender: Any) {
         PresenterManager.shared.show(vc: .home)
