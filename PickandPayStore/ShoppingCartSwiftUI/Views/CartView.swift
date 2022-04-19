3922fb0c2facbf4b9164c33d382da64f90667691
Costin79 <costpopr@protonmail.com>
2022-04-19 09:50:16 -0500

    Notification to log in added to the cart view

--- PickandPayStore/ShoppingCartSwiftUI/Views/CartView.swift
+++ PickandPayStore/ShoppingCartSwiftUI/Views/CartView.swift
@@ -9,41 +9,68 @@ import SwiftUI
 
 struct CartView: View {
     
+    // The check if the client is logged in.
+    @State var isLoggedIn: Bool = UserSessionManager.shared.isLoggedIn()
+    
+    // This state variable controlls the display of the NotRegisteredNotification.
+    @State var displayNotification: Bool = false
+    
     // Environment object modifier of CartManager type.
     @State var productsInCart: [Product] = []
     @State var numberInCart: Int = 0
     
     var body: some View {
         
-        ScrollView {
-            
-            if productsInCart.count > 0
-            {
-                HStack {
-                    Text("Your cart total is")
-                    Spacer()
-                    Text("$\(CartManager.sharedCart.total, specifier: "%.2f")")
-                        .bold()
-                }
-                .padding()
+        ZStack {
+            ScrollView {
                 
-                ForEach(productsInCart, id: \.id){
-                    product in
-                    ProductInCart(productsInCart: $productsInCart, numberInCart: $numberInCart, product: product)
-                }
-        
-            }
-            else
-            {
-                Text("Your cart is empty")
-            }
+                if productsInCart.count > 0
+                {
+                    VStack {
+                        HStack {
+                            Text("Your cart total is")
+                            Spacer()
+                            Text("$\(CartManager.sharedCart.total, specifier: "%.2f")")
+                                .bold()
+                        }
+                        .padding()
+                        
+                        HStack {
+                            Button ( action: {
+                                displayNotification.toggle()
+                            }, label: {
+                                Text("Check Out")
+                                    .foregroundColor(.black)
+                                    .fontWeight(.bold)
+                                    .padding()
+                                    .border(.red, width: 2)
+                                })
+                            .padding()
+                            
+                            Spacer()
+                        }
+                        
+                    }
+                    ForEach(productsInCart, id: \.id){
+                        product in
+                        ProductInCart(productsInCart: $productsInCart, numberInCart: $numberInCart, product: product)
+                    }
             
-        }
-        .navigationTitle(Text("My Cart"))
-        .padding(.top)
-        .onAppear(){
-            productsInCart = CartManager.sharedCart.products
-            numberInCart = CartManager.sharedCart.products.count
+                }
+                else
+                {
+                    Text("Your cart is empty")
+                }
+                
+            }
+            .navigationTitle(Text("My Cart"))
+            .padding(.top)
+            .onAppear(){
+                productsInCart = CartManager.sharedCart.products
+                numberInCart = CartManager.sharedCart.products.count
+            }
+            displayNotification ? NotRegisteredNotification() : nil
+            
         }
     }
 }
